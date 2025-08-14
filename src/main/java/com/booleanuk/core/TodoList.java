89bc72710d38b7919c70483b45f35bb831813282
David Stern <davidestern@icloud.com>
2025-08-14 15:33:25 +0200

    Core completed

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,7 +1,6 @@
 package com.booleanuk.core;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TodoList {
 
@@ -12,35 +11,98 @@ public class TodoList {
     }
 
     public Boolean addTask(String taskName) {
-        return null;
+        taskName = taskName.strip().toLowerCase();
+        if (taskName.isBlank()) {
+            return null;
+        }
+
+        for (String key : tasks.keySet()) {
+            if (taskName.equals(key.toLowerCase())) {
+                return false;
+            }
+        }
+        tasks.put(taskName, false); // not completed by default
+        return true;
     }
 
     public List<String> getTasks() {
-        return null;
+        return new ArrayList<>(tasks.keySet());
     }
 
     public List<String> getCompletedTasks() {
-        return null;
+        List<String> completedTasks = new ArrayList<>();
+        for (String key : tasks.keySet()) {
+            if (tasks.get(key)) {
+                completedTasks.add(key);
+            }
+        }
+        return completedTasks;
     }
 
     public List<String> getIncompleteTasks() {
-        return null;
+        List<String> incompleteTasks = new ArrayList<>();
+        for (String key : tasks.keySet()) {
+            if (!tasks.get(key)) {
+                incompleteTasks.add(key);
+            }
+        }
+        return incompleteTasks;
     }
 
     public Boolean changeTaskStatus(String taskName) {
-        return null;
+        taskName = taskName.strip().toLowerCase();
+        if (taskName.isBlank())
+            return false;
+
+        for (String key : tasks.keySet()) {
+            if (taskName.equals(key.toLowerCase())) {
+                tasks.put(key, !tasks.get(key));
+                return true;
+            }
+        }
+        return false;
     }
 
     public List<String> searchTask(String searchString) {
-        return null;
+        List<String> searchResults = new ArrayList<>();
+
+        for (String key : tasks.keySet()) {
+            if (key.toLowerCase().contains(searchString)) {
+                searchResults.add(key);
+            }
+        }
+
+        if (searchResults.isEmpty()) {
+            searchResults.add("No tasks found");
+        }
+
+        return searchResults;
     }
 
     public Boolean removeTask(String taskName) {
-        return null;
+        taskName = taskName.strip().toLowerCase();
+        if (taskName.isBlank()) {
+            return false;
+        }
+
+        for (String key : tasks.keySet()) {
+            if (taskName.equals(key.toLowerCase())) {
+                tasks.remove(key);
+                return true;
+            }
+        }
+        return false;
     }
 
     public List<String> getAllTasksOrdered(boolean descending) {
-        return null;
+
+        List<String> sortedTasks = new ArrayList<>(tasks.keySet());
+
+        if (!descending) {
+            return sortedTasks.stream().sorted().toList();
+        } else {
+            return sortedTasks.stream().sorted().toList().reversed();
+        }
     }
 
 }
