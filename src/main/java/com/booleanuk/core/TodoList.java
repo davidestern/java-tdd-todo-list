20a16319f26063d23a7e88ce57276f918f820264
David Stern <davidestern@icloud.com>
2025-08-14 14:30:47 +0200

    Skeleton for TodoList created, all tests for TodoList written based on domain model

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,46 @@
 package com.booleanuk.core;
 
+import java.util.List;
+import java.util.Map;
+
 public class TodoList {
+
+    Map<String, Boolean> tasks;
+
+    public TodoList(Map<String, Boolean> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Boolean addTask(String taskName) {
+        return null;
+    }
+
+    public List<String> getTasks() {
+        return null;
+    }
+
+    public List<String> getCompletedTasks() {
+        return null;
+    }
+
+    public List<String> getIncompleteTasks() {
+        return null;
+    }
+
+    public Boolean changeTaskStatus(String taskName) {
+        return null;
+    }
+
+    public List<String> searchTask(String searchString) {
+        return null;
+    }
+
+    public Boolean removeTask(String taskName) {
+        return null;
+    }
+
+    public List<String> getAllTasksOrdered(boolean descending) {
+        return null;
+    }
 
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,269 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class TodoListTest {
+
+    Map<String, Boolean> tasks = null;
+
+    /*
+    addTask(String taskName)
+     */
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void addTaskNotAlreadyInTaskListShouldReturnTrue() {
+        this.tasks = new HashMap<>();
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(true, tl.addTask("Eat"));
+        Assertions.assertEquals(true, tl.addTask("Work out"));
+        Assertions.assertEquals(true, tl.addTask("Do some programming"));
+    }
+
+    @Test
+    public void addTaskAlreadyInTaskListShouldReturnFalse() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(false, tl.addTask("Eat"));
+        Assertions.assertEquals(false, tl.addTask("eat"));
+        Assertions.assertEquals(false, tl.addTask("Work out"));
+        Assertions.assertEquals(false, tl.addTask("work out"));
+    }
+
+    @Test
+    public void addTaskInvalidNameShouldReturnNull() {
+        this.tasks = new HashMap<>();
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertNull(tl.addTask(" "));
+        Assertions.assertNull(tl.addTask("    "));
+        Assertions.assertNull(tl.addTask(""));
+    }
+
+    /*
+    getTasks()
+     */
+    @Test
+    public void getTasksTaskMapNotEmpty() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of("Eat", "Work out"), tl.getTasks());
+        Assertions.assertEquals(List.of("eat", "work out"), tl.getTasks());
+    }
+
+    @Test
+    public void getTasksWhenMapIsEmpty() {
+        this.tasks = new HashMap<>();
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of(), tl.getTasks());
+    }
+
+    /*
+    getCompletedTasks()
+     */
+    @Test
+    public void getCompletedTasksWhenCompletedTasksExist() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", true);
+        tasks.put("Work out", true);
+        tasks.put("Do some programming", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of("Eat", "Work out"), tl.getCompletedTasks());
+        Assertions.assertEquals(List.of("eat", "work out"), tl.getCompletedTasks());
+    }
+
+    @Test
+    public void getCompletedTasksWhenCompletedTasksDontExist() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming", true);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of(), tl.getCompletedTasks());
+    }
+
+    /*
+    getIncompleteTasks
+     */
+    @Test
+    public void getIncompleteTasksWhenCompletedTasksExist() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", true);
+        tasks.put("Work out", true);
+        tasks.put("Do some programming", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of("Do some programming"), tl.getIncompleteTasks());
+        Assertions.assertEquals(List.of("do some programming"), tl.getIncompleteTasks());
+    }
+
+    @Test
+    public void getIncompleteTasksWhenCompletedTasksDontExist() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming", true);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of("Eat", "Work out"), tl.getIncompleteTasks());
+        Assertions.assertEquals(List.of("eat", "work out"), tl.getIncompleteTasks());
+    }
+
+    /*
+    changeTaskStatus(String taskName)
+     */
+    @Test
+    public void changeTaskStatusWhenTaskExistsAndIsIncomplete() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(true, tl.changeTaskStatus("Eat"));
+        Assertions.assertEquals(true, tasks.get("Eat"));
+        Assertions.assertEquals(true, tl.changeTaskStatus("eat"));
+        Assertions.assertEquals(false, tasks.get("Eat"));
+    }
+
+    @Test
+    public void changeTaskStatusWhenTaskExistsAndIsCompleted() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", true);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(true, tl.changeTaskStatus("Eat"));
+        Assertions.assertEquals(false, tasks.get("Eat"));
+        Assertions.assertEquals(true, tl.changeTaskStatus("eat"));
+        Assertions.assertEquals(true, tasks.get("Eat"));
+    }
+
+    @Test
+    public void changeTaskStatusWhenTasksDoesNotExist() {
+        this.tasks = new HashMap<>();
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(false, tl.changeTaskStatus("Eat"));
+        Assertions.assertEquals(false, tl.changeTaskStatus("work out"));
+    }
+
+    /*
+    searchTask(String searchString)
+     */
+    @Test
+    public void searchTaskResultFound() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming today", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of("Eat", "Work out", "Do some programming"), tl.searchTask("t"));
+        Assertions.assertEquals(List.of("Work out", "Do some programming"), tl.searchTask("o"));
+        Assertions.assertEquals(List.of("Eat"), tl.searchTask("eat"));
+    }
+
+    @Test
+    public void searchTaskResultNotFound() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming today", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of(), tl.searchTask("z"));
+        Assertions.assertEquals(List.of(), tl.searchTask("zx"));
+        Assertions.assertEquals(List.of(), tl.searchTask(":123!"));
+    }
+
+    /*
+    removeTask(String taskName)
+     */
+    @Test
+    public void removeTaskThatExists() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming today", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(true, tl.removeTask("Eat"));
+        Assertions.assertEquals(true, tl.removeTask("work out"));
+        Assertions.assertEquals(true, tl.removeTask("Do some programming Today"));
+    }
+
+    @Test
+    public void removeTaskThatDoesNotExist() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(false, tl.removeTask("work out"));
+        Assertions.assertEquals(false, tl.removeTask("Do some programming Today"));
+    }
+
+    /*
+    getAllTasksOrdered(boolean descending)
+     */
+    @Test
+    public void getAllTasksOrderedWhenNoTasksExist() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming today", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of(), tl.getAllTasksOrdered(false));
+        Assertions.assertEquals(List.of(), tl.getAllTasksOrdered(true));
+    }
+
+    @Test
+    public void getAllTasksOrderedAscending() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming today", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of("Do some programming today", "Eat", "Work out"), tl.getAllTasksOrdered(false));
+    }
+
+    @Test
+    public void getAllTasksOrderedDescending() {
+        this.tasks = new HashMap<>();
+        tasks.put("Eat", false);
+        tasks.put("Work out", false);
+        tasks.put("Do some programming today", false);
+
+        TodoList tl = new TodoList(tasks);
+
+        Assertions.assertEquals(List.of("Work out", "Eat", "Do some programming today"), tl.getAllTasksOrdered(true));
     }
 }
